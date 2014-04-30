7716169caa1549bed7c4b72d110aaa8457ff7da5
mattiasdelang <mattiasdelang@gmail.com>
2014-04-30 16:43:02 +0200

    juiste sqlfile toegevoegd

--- rest/mysql/rest_app.sql
+++ rest/mysql/rest_app.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Gegenereerd op: 29 apr 2014 om 19:05
+-- Gegenereerd op: 30 apr 2014 om 16:38
 -- Serverversie: 5.6.16
 -- PHP-versie: 5.5.9
 
@@ -19,6 +19,23 @@ SET time_zone = "+00:00";
 --
 -- Databank: `rest_app`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `menu`
+--
+
+CREATE TABLE IF NOT EXISTS `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `naam` varchar(255) NOT NULL,
+  `omschrijving` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `prijs` decimal(5,2) NOT NULL,
+  `showmenu` tinyint(1) NOT NULL DEFAULT '1',
+  `restaurantid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 -- --------------------------------------------------------
 
@@ -44,17 +61,9 @@ CREATE TABLE IF NOT EXISTS `restaurant` (
   `zaterdag` text NOT NULL,
   `zondag` text NOT NULL,
   `restaurantownerid` int(11) NOT NULL,
-  `showres` tinyint(1) NOT NULL,
+  `showres` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
--- Gegevens worden uitgevoerd voor tabel `restaurant`
---
-
-INSERT INTO `restaurant` (`id`, `naam`, `adres`, `gemeente`, `specialiteit`, `website`, `facebook`, `mail`, `telnr`, `maandag`, `dinsdag`, `woensdag`, `donderdag`, `vrijdag`, `zaterdag`, `zondag`, `restaurantownerid`, `showres`) VALUES
-(3, 't spaans dak', 'zoetwaterdreef 12', 'oud-heverlee', 'omelet nature', 'www.spaansdak.be', 'www.facebook.com/spaansdak', 'mattiasdelang@hotmail.com', '01635926', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', 'gesloten', 60, 0),
-(4, 't spaans dak', 'zoetwaterdreef 12', 'oud-heverlee', 'omelet nature', 'www.spaansdak.be', 'www.facebook.com/spaansdak', 'mattiasdelang@hotmail.com', '01635926', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', '10.00-12.00/15.30-21.00', 60, 0);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
@@ -71,14 +80,7 @@ CREATE TABLE IF NOT EXISTS `restaurantowner` (
   `randomnr` varchar(255) NOT NULL,
   `verif` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=61 ;
-
---
--- Gegevens worden uitgevoerd voor tabel `restaurantowner`
---
-
-INSERT INTO `restaurantowner` (`id`, `firstname`, `lastname`, `email`, `password`, `randomnr`, `verif`) VALUES
-(60, 'mattias', 'delang', 'mattiasdelang@hotmail.com', '64ebaa3c197068d305d7fa4c7d186eb5', 't5a21bb59cbd6d3bce340adfc8b2b416', 1);
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=62 ;
 
 -- --------------------------------------------------------
 
@@ -92,17 +94,9 @@ CREATE TABLE IF NOT EXISTS `tafel` (
   `plaatsen` int(11) NOT NULL,
   `restaurantid` int(11) NOT NULL,
   `ownerid` int(11) NOT NULL,
+  `tafelshow` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Gegevens worden uitgevoerd voor tabel `tafel`
---
-
-INSERT INTO `tafel` (`id`, `tafelnummer`, `plaatsen`, `restaurantid`, `ownerid`) VALUES
-(1, 5, 3, 1, 1),
-(2, 5, 4, 1, 1),
-(3, 5, 6, 1, 1);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
