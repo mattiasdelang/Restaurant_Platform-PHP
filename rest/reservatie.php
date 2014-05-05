c06193644b37e0be8e59ab45726cfa29f3e25746
dietermeys <dietermeys@gmail.com>
2014-05-05 21:33:41 +0200

    reservatie aangemaakt

---
+++ rest/reservatie.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Dieter
+ * Date: 3/05/14
+ * Time: 20:55
+ * To change this template use File | Settings | File Templates.
+ */
+
+?>
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+
+</body>
+</html>
